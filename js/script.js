ba017496b700827e93ea925d28500f38371c4f3b
niprestige <prestigalain@gmail.com>
2022-09-16 21:50:41 +0200

    clouds

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
 });
+
+
+VANTA.CLOUDS({
+  el: "#hero_3d",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  cloudColor: 0x94a5c6
+})
